62d0ecb1a19e2547f7789ed800a6df7518f43432
yasuo3 <sungdm0303@gmail.com>
2020-07-12 12:26:11 +0900

    Update todolist.js

--- to-do-list/todolist.js
+++ to-do-list/todolist.js
@@ -1,32 +1,46 @@
+let inputValue;    // 입력값 저장 변수
+let checkbox;       // 체크박스
+let outputLabel;    // 입력값 출력 라벨
+let deleteButton; // 삭제 버튼
+
 function pressEnter(){
     if(event.keyCode == 13){
-        let iv = document.getElementById('todo').value;    // 입력값 저장 변수
-        let condiv = document.createElement('div');        // 컨테이너 디브
-        let check = document.createElement('input');       // 체크박스
-        check.type = 'checkbox';
-        let addlabel = document.createElement('label');    // 입력값 출력 라벨
-        let delete_btn = document.createElement('button'); // 삭제 버튼
-        addlabel.innerHTML = iv;
-        condiv.setAttribute('id', iv);
-        check.setAttribute('id', iv + 1);
-        delete_btn.innerHTML = '삭제';
+        let container = document.createElement('div');
+        inputValue = document.getElementById('todo').value;
+        checkbox = document.createElement('input');
+        outputLabel = document.createElement('label');
+        deleteButton = document.createElement('button');
+        checkbox.type = 'checkbox';
+        
+        outputLabel.innerHTML = inputValue;
+        container.setAttribute('id', inputValue);
+        checkbox.setAttribute('id', inputValue + 1);
+        deleteButton.innerHTML = '삭제';
 
-        condiv.appendChild(check);
-        condiv.appendChild(addlabel);
-        condiv.appendChild(delete_btn);
+        container.appendChild(checkbox);
+        container.appendChild(outputLabel);
+        container.appendChild(deleteButton);
 
-        document.getElementById("list_container").appendChild(condiv);
+        document.getElementById("list_container").appendChild(container);
         document.getElementById("todo").value = "";
 
-        check.addEventListener("change", function(e){                       //완료 함수
-            let chId = e.target.id;
-            let getDiv = document.getElementById(chId.slice(0, -1));
-            getDiv.classList.toggle("active");
-        })
+        checkbox.addEventListener("change", strikeThrough);
 
-        delete_btn.addEventListener("click", function(e){
-            let parent = document.getElementById('list_container');         //삭제 함수
-            parent.removeChild(condiv);
-        })
+        // deleteButton.addEventListener("click", function(e){
+        //     let toDoList = document.getElementById('list_container');         //삭제 함수
+        //     toDoList.removeChild(container);
+        // })
+
+        deleteButton.addEventListener("click", deleteElement);
     }
+}
+
+function strikeThrough(e){
+    let getDiv = document.getElementById(e.target.id.slice(0, -1));
+    getDiv.classList.toggle("active");
+}
+
+function deleteElement(e){
+    let toDoList = document.getElementById('list_container');
+    toDoList.removeChild(this.parentElement);
 }
